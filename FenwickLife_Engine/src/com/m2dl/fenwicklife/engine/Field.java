aab4e3aaac60f31e3565dc3b3093427b3a4c031c
Vincent Carassus <vincentcarassus@gmail.com>
2014-05-23 17:07:31 +0200

    Refactored Field & added param testing

--- FenwickLife_Engine/src/com/m2dl/fenwicklife/engine/Field.java
+++ FenwickLife_Engine/src/com/m2dl/fenwicklife/engine/Field.java
@@ -6,6 +6,13 @@ import java.util.Map;
 import com.m2dl.fenwicklife.Position;
 
 public class Field {
+	public static final int MIN_SIZE_X = 10;
+	public static final int MIN_SIZE_Y = 10;
+	public static final int MAX_SIZE_X = 300;
+	public static final int MAX_SIZE_Y = 300;
+	public static final int MAX_CENTRAL_WALL_SIZE = 100;
+	public static final int MIN_CENTRAL_WALL_SIZE = 1;
+	
 	private Map<Position, Tile> grid;
 	private int firstCorridorY;
 	private int secondCorridorY;
@@ -14,14 +21,64 @@ public class Field {
 	private int centerWallXLeft;
 	private int centerWallXRight;
 	
-	public boolean isObstacle(int x, int y) {
-		return isObstacle(new Position(x, y));
+	public Field(int sizeX, int sizeY, int centerWallSize, int firstCorridorY, int secondCorridorY) {
+		super();
+		if(sizeX < MIN_SIZE_X || sizeX > MAX_SIZE_X) {
+			throw new RuntimeException("Field Size X invalid");
+		}
+		
+		if(sizeY < MIN_SIZE_Y || sizeY > MAX_SIZE_Y) {
+			throw new RuntimeException("Field Size Y invalid");
+		}
+		
+		if(centerWallSize < MIN_CENTRAL_WALL_SIZE || centerWallSize > MAX_CENTRAL_WALL_SIZE) {
+			throw new RuntimeException("Center wall size invalid");
+		}
+		
+		if(firstCorridorY == secondCorridorY) {
+			throw new RuntimeException("Both corridors on same coordinate");
+		}
+		
+		if(firstCorridorY < 0 || secondCorridorY < 0 || firstCorridorY > sizeY || secondCorridorY > sizeY) {
+			throw new RuntimeException("Invalid corridor coordinates");
+		}
+		
+		this.firstCorridorY = firstCorridorY;
+		this.secondCorridorY = secondCorridorY;
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+		this.centerWallXLeft   = sizeX / 2 - centerWallSize;
+		this.centerWallXRight  = sizeX / 2 + centerWallSize;
+		this.grid = new HashMap<Position, Tile>();
 	}
 	
-	public boolean isObstacle(Position p) {
-		return grid.get(p).getType() != TileType.EMPTY;
+	public int getFirstCorridorY() {
+		return firstCorridorY;
 	}
 	
+	public Map<Position, Tile> getGrid() {
+		return this.grid;
+	}
+	
+	public int getSecondCorridorX() {
+		return secondCorridorY;
+	}
+	
+	public Tile[][] getSurroundings(int x, int y) {
+		return getSurroundings(x, y, 3);
+	}
+
+	public Tile[][] getSurroundings(int x, int y, int size) {
+		Tile[][] tilesSubGrid = new Tile[7][7];
+		for( int i = 0; i < 7; i++) {
+			for( int j = 0; j < 7; j++) {
+				TileType type = getTileType(x-size+i, y-size+j);
+				tilesSubGrid[i][j] = new Tile(x-size+i, y-size+j, type);
+			}
+		}
+		return tilesSubGrid;
+	}
+
 	public TileType getTileType(int x, int y) {
 		return getTileType(new Position(x, y));
 	}
@@ -41,55 +98,19 @@ public class Field {
 		return TileType.EMPTY;
 	}
 	
-	public Map<Position, Tile> getGrid() {
-		return this.grid;
+	public boolean isObstacle(int x, int y) {
+		return isObstacle(new Position(x, y));
+	}
+	
+	public boolean isObstacle(Position p) {
+		return grid.get(p).getType() != TileType.EMPTY;
 	}
 
-	public Field(int sizeX, int sizeY, int centerWallSize, int firstCorridorY, int secondCorridorY) {
-		super();
-		
-		if(firstCorridorY == secondCorridorY) {
-			throw new RuntimeException("Both corridors on same coordinate");
-		}
-		
-		if(firstCorridorY < 0 || secondCorridorY < 0 || firstCorridorY > sizeY || secondCorridorY > sizeY) {
-			throw new RuntimeException("Invalid corridor coordinates");
-		}
-		
-		this.firstCorridorY = firstCorridorY;
-		this.secondCorridorY = secondCorridorY;
-		this.sizeX = sizeX;
-		this.sizeY = sizeY;
-		this.centerWallXLeft   = sizeX / 2 - centerWallSize;
-		this.centerWallXRight  = sizeX / 2 + centerWallSize;
-		this.grid = new HashMap<Position, Tile>();
-	}
-
-	public int getFirstCorridorY() {
-		return firstCorridorY;
-	}
 	public void setFirstCorridorY(int firstCorridorY) {
 		this.firstCorridorY = firstCorridorY;
 	}
-	public int getSecondCorridorX() {
-		return secondCorridorY;
-	}
+	
 	public void setSecondCorridorX(int secondCorridorX) {
 		this.secondCorridorY = secondCorridorX;
-	}
-
-	public Tile[][] getSurroundings(int x, int y) {
-		return getSurroundings(x, y, 3);
-	}
-	
-	public Tile[][] getSurroundings(int x, int y, int size) {
-		Tile[][] tilesSubGrid = new Tile[7][7];
-		for( int i = 0; i < 7; i++) {
-			for( int j = 0; j < 7; j++) {
-				TileType type = getTileType(x-size+i, y-size+j);
-				tilesSubGrid[i][j] = new Tile(x-size+i, y-size+j, type);
-			}
-		}
-		return tilesSubGrid;
 	}
 }
